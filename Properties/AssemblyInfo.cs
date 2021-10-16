076ec627f8ce56d206c8e7998e61ab09eab3fba5
Dorigon Granato <granatos@gmail.com>
2021-10-16 09:54:41 -0300

    Nome do projeto alterado

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associadas a um assembly.
-[assembly: AssemblyTitle("TesteTemperaturaPCNetFull")]
+[assembly: AssemblyTitle("SerialTemperature")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TesteTemperaturaPCNetFull")]
+[assembly: AssemblyProduct("SerialTemperature")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
